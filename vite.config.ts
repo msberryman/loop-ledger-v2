421841920916a741eac3c8816042bd32be003040
Matt <msberryman1@gmail.com>
2026-01-15 17:35:05 -0600

    Update Vite base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  base: "/",
   plugins: [react()],
-  base: mode === 'production' ? '/loop-ledger-v2/' : '/',
-}));
+});
+
